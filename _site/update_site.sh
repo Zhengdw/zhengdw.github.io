536808db2328e42bc55030292a8fef3cd862e8cc
zhengdw <7789863+Zhengdw@users.noreply.github.com>
2024-01-23 16:09:44 -0600

    uploaded slides for auction alg

--- _site/update_site.sh
+++ _site/update_site.sh
@@ -1,3 +1,4 @@
 bundle exec jekyll build
+echo "$(echo '<meta http-equiv="refresh" content="0; URL=https://zhengdw.github.io/"/>' ; cat _site/index.html)" > _site/index.html
 scp -r _site davidzheng@18.220.149.166:~/
 ssh davidzheng@18.220.149.166 "rm -rf old_public_html && mv public_html old_public_html && mv _site/ public_html"
